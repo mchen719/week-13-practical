10c8c11ab2c3890920586b1815538868ade1d003
Matthew Chen <m.chen71996@mail.com>
2023-12-16 18:10:41 -0500

    added files

---
+++ hw-part-one/server.js
@@ -0,0 +1,15 @@
+const express = require('express') // imported express from node modules
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send(`Hello stranger!`)
+})
+
+app.get('/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`)
+    })
+
+app.listen(3000, () => { 
+    console.log('Connected')
+    })
