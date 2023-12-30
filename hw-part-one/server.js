a33a56a20a9b8ac8d5d67c592e68d10e2ff3788b
Matthew Chen <m.chen71996@mail.com>
2023-12-29 19:33:50 -0500

    Tip Calculator express application created.

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -10,7 +10,21 @@ app.get('/:name', (req, res) => {
     res.send(`Hello ${req.params.name}`)
     })
 
-
+    app.get('/tip/:total/:tipPercentage', (req, res) => {
+        const { total, tipPercentage } = req.params;
+      
+        const totalAmount = parseFloat(total);
+        const tipPercentageValue = parseFloat(tipPercentage);
+    
+        if (isNaN(totalAmount) || isNaN(tipPercentageValue)) {
+          return res.status(400).send('Invalid input. Please provide valid numbers.');
+        }
+      
+        const tipAmount = (totalAmount * tipPercentageValue) / 100;
+      
+        res.send(`Tip amount:$${tipAmount}`);
+      });
+      
 
     
 app.listen(3000, () => { 
